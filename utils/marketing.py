98d063302053508a21504c74aed48ca1f70c77b6
Louis Brulé Naudet <louisbrulenaudet@icloud.com>
2024-04-08 15:38:10 +0200

    documentation update

--- utils/marketing.py
+++ utils/marketing.py
@@ -10,15 +10,40 @@ client = MistralClient(api_key=os.getenv("MISTRAL_API_KEY"))
 
 def plan(metadata: str) -> dict:
   """
-  Takes a README and extracts the following properties
-  
-  Parameters:
-      readme (str): The README content
+  Extracts relevant platforms from a README.
 
-  Returns:
-      dict: A dictionary containing the extracted properties
+  This function takes a `metadata` string representing the content of a README
+  and extracts relevant platforms for promoting a project. It interacts with
+  MistralAI service to generate responses based on the provided metadata.
+
+  Parameters
+  ----------
+  metadata : str
+      The content of the README.
+
+  Returns
+  -------
+  dict
+      A dictionary containing the extracted properties. The dictionary has
+      a key "platforms" that holds a list of strings designating the different
+      possible platforms for promoting the project. The platforms can be
+      ['Hackernews', 'Blog', 'Reddit', 'Twitter', 'ProductHunt'].
+
+  Notes
+  -----
+  This function utilizes MistralAI's chat completion model to generate responses
+  based on the provided metadata. The extracted platforms are returned in a
+  JSON object format.
+
+  Raises
+  ------
+  (Any exceptions that may be raised during the execution of the function)
+
+  Examples
+  --------
+  >>> plan("README content goes here...")
+  {'platforms': ['Hackernews', 'Blog']}
   """
-  
   system_prompt = """
 ## Context
 This is a marketing application aimed to generate omnichannels campaign for open-source projects across various platforms and content types (Hackernews posts, Reddit posts, Twitter/X posts, Youtube video tutorials, Hackathons, ...)
@@ -35,7 +60,7 @@ JSON object following format:
 """.strip()
   max_retries = 3
   retry_count = 0
-  
+
   try:
     messages = [
       ChatMessage(role="system", content=system_prompt),
